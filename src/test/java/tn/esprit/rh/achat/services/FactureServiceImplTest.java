d8314b7678829fbecfa0b0f7a253c6aec1106583
Mohamed Bader Moussa <mohamedbader.moussa@esprit.tn>
2022-11-10 21:47:13 +0100

    sonar tests code cleaning

--- src/test/java/tn/esprit/rh/achat/services/FactureServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceImplTest.java
@@ -14,27 +14,27 @@ import java.util.Date;
 @Slf4j
 class FactureServiceImplTest {
 
-    @Autowired
-    IFactureService factureService;
-
-    @Test
-    @Order(0)
-    void TestAddFacture() {
+//    @Autowired
+//    IFactureService factureService;
+//
+//    @Test
+//    @Order(0)
+//    void TestAddFacture() {
 //        Facture facture = factureService.addFacture(Facture.builder()
 //                .dateCreationFacture(new Date())
 //                .archivee(false)
 //                .build()
 //        );
 //        Assertions.assertNotNull(facture);
-    }
-
-    @Test
-    @Order(1)
-    void TestRetrieveAllFactures() {
+//    }
+//
+//    @Test
+//    @Order(1)
+//    void TestRetrieveAllFactures() {
 //        int sizeFactures = factureService.retrieveAllFactures().size();
 //        factureService.addFacture(factureService.addFacture(Facture.builder().montantFacture(300F).dateCreationFacture(new Date()).build()));
 //        log.info("Size:"+String.valueOf(sizeFactures));
 //        Assertions.assertEquals(sizeFactures+1, factureService.retrieveAllFactures().size());
-    }
+//    }
 
 }
